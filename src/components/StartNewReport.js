1dc0bd000f94c9160a7ea376551eb2af872ef7bb
Viktoria Herzer <viktoriaherzer@Viktorias-MacBook-Pro.local>
2019-09-19 00:49:13 +0200

    QR code scanner works

--- src/components/StartNewReport.js
+++ src/components/StartNewReport.js
@@ -1,42 +1,19 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Image, Button, TouchableOpacity, Linking } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Image, Button, TouchableOpacity, Linking } from 'react-native';
 import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 
-class StartNewReport extends React.Component {
+class StartNewReport extends Component {
 
 /*TEST QR CODE SCANNER*/
-  constructor(){
-    super();
-    this.state ={
-      QR_Code_Value: '',
-      Start_Scanner: false,
-    };
-  }
-
-  openLink_in_browser = () => {
-    Linking.openURL(this.state.QR_Code_Value);
-  }
-
-  onQR_Code_Scan_Done = (QR_Code) =>{
-    this.setState({QR_Code_Value: QR_Code});
-    this.setState({Start_Scanner: false});
-  }
-
-  open_QR_Code_Scanner = ()=> {
-    var that = this;
-    if (Platform.OS === 'android'){
-
-    } else{
-      that.setState({QR_Code_Value:''});
-      that.setState({Start_Scanner:true});
-    }
+  onSuccess = (e) => {
+    Linking
+    .openURL(e.data)
+    .catch(err => console.error('an error occured'));
   }
 
   render(){
-
-    if (!this.state.Start_Scanner){
-
       return (
         <View style={styles.startnewreportContainer}>
           <View>
@@ -56,40 +33,25 @@ class StartNewReport extends React.Component {
             <Text style={{fontSize:22, textAlign:'center', color: '#FFFFFF', paddingTop: 15}}>Scannen Sie den QR-Code</Text>
 
             <View style={styles.MainContainer}>
-              <Text style={styles.QR_text}>
-                {this.state.QR_Code_Value ? 'Scanned QR Code: ' + this.state.QR_Code_Value : ''}
-              </Text>
-
-              {this.state.QR_Code_Value.includes("http") ?
-                <TouchableOpacity onPress={this.openLink_in_browser} style={styles.button}>
-                  <Text> Open link in default Browser </Text>
-                </TouchableOpacity> : null
-              }
-
-              <TouchableOpacity onPress={this.open_QR_Code_Scanner} style={styles.button}>
-                  <Text> Open QR Code Scanner </Text>
-              </TouchableOpacity>
+              <QRCodeScanner
+                onRead={this.onSuccess}
+                topContent={
+                  <Text style={styles.centerText}>
+                    Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                  </Text>
+                }
+                bottomContent={
+                  <TouchableOpacity style={styles.buttonTouchable}>
+                    <Text style={styles.buttonText}>OK. Got it!</Text>
+                  </TouchableOpacity>
+                }
+              />
             </View>
 
           </View>
         </View>
       );
     }
-    return (
-      <View style={{flex:1}}>
-        <CameraKitCameraScreen
-          showFrame={true}
-          scanBarcode={true}
-          laserColor={'#FF3D00'}
-          frameColor={'#00C853'}
-          colorForScannerFrame={'black'}
-          onReadCode={event =>
-            this.onQR_Code_Scan_Done(event.nativeEvent.codeStringValue)
-          }
-        />
-      </View>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
@@ -105,19 +67,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#2B2B2B'
   },
-  QR_text:{
-    color: '#000',
-    fontSize: 19,
-    padding: 8,
-    marginTop:12
-  },
-  button:{
-    backgroundColor:'#2979FF',
-    alignItems: 'center',
-    padding: 12,
-    width: 300,
-    marginTop: 14
-  },
+  centerText: {
+      flex: 1,
+      fontSize: 18,
+      padding: 32,
+      color: '#777',
+    },
+    textBold: {
+      fontWeight: '500',
+      color: '#000',
+    },
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+      padding: 16,
+    },
   startnewreportContainer:{
     backgroundColor: "#252525"
   }
